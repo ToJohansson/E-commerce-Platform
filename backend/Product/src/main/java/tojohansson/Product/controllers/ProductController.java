d4256722de75ea80c57d4903fccdff608ff5e261
Tobias Johansson <tobiasp91@hotmail.com>
2024-06-13 12:09:05 +0200

    Controller with corresponding rest api endpoints for Product. updated EntityNotFoundException to be more generic. changed createProduct to handle a new product with jpa.

---
+++ backend/Product/src/main/java/tojohansson/Product/controllers/ProductController.java
@@ -0,0 +1,48 @@
+package tojohansson.Product.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import tojohansson.Product.dto.ProductDto;
+import tojohansson.Product.models.Product;
+import tojohansson.Product.services.ProductService;
+
+import java.util.List;
+
+@Controller(value = "/api/products")
+@CrossOrigin
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    public ProductController() {
+    }
+    @GetMapping("/")
+    public List<ProductDto> getAllProducts(){
+        return productService.getAllProducts();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> productById(@PathVariable("id") Long id){
+        ProductDto dto = productService.getProductById(id);
+        return new ResponseEntity<ProductDto>(dto, HttpStatus.OK);
+    }
+    @PostMapping("/")
+    public ResponseEntity<Product> createProduct(@RequestBody ProductDto dto){
+        return new ResponseEntity<>(productService.createProduct(dto),HttpStatus.CREATED);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") Long id, @RequestBody ProductDto dto) {
+        dto.setId(id); // Ensure ID consistency
+        Product updatedProduct = productService.updateProduct(dto);
+        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable("id") Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+}
