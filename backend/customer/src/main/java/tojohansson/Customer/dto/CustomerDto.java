2a1ab7ac17e17f95cb931a185813c55419dff5e1
Tobias Johansson <tobiasp91@hotmail.com>
2024-06-17 09:49:56 +0200

    model and dto for customer entity

---
+++ backend/customer/src/main/java/tojohansson/Customer/dto/CustomerDto.java
@@ -0,0 +1,43 @@
+package tojohansson.customer.dto;
+
+public class CustomerDto {
+
+    private Long id;
+
+    private String name;
+    private String address;
+    private String mail;
+
+    public CustomerDto(){}
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
