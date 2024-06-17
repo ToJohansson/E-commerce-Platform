fc3e640b9b1a4684857fa74d18c6870ecdd42be8
Tobias Johansson <tobiasp91@hotmail.com>
2024-06-17 10:23:40 +0200

    generic customer id check in service layer. delete and update customer

--- backend/customer/src/main/java/tojohansson/Customer/services/CustomerService.java
+++ backend/customer/src/main/java/tojohansson/Customer/services/CustomerService.java
@@ -3,6 +3,7 @@ package tojohansson.customer.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tojohansson.customer.dto.CustomerDto;
+import tojohansson.customer.exceptions.EntityNotFoundException;
 import tojohansson.customer.models.Customer;
 import tojohansson.customer.repositories.CustomerRepository;
 
@@ -16,7 +17,7 @@ public class CustomerService {
     }
 
     // post
-    public Customer createCustomer(CustomerDto cDto){
+    public Customer createCustomer(CustomerDto cDto) {
         Customer customer = (mapToCustomer(cDto, new Customer()));
         return customerRepository.save(customer);
     }
@@ -24,19 +25,32 @@ public class CustomerService {
     // get
 
     // put
+    public Customer updateCustomer(CustomerDto customerDto){
+        Customer customer = checkCustomerById(customerDto.getId());
+        customer = (mapToCustomer(customerDto, customer));
+        return customerRepository.save(customer);
+    }
 
     // delete
-    public void deleteCustomer(Long id){
+    public void deleteCustomer(Long id) {
+        Customer customer = checkCustomerById(id);
+        customerRepository.deleteById(customer.getId());
 
     }
 
-    private Customer mapToCustomer(CustomerDto cDto, Customer customer){
+    private Customer checkCustomerById(Long id){
+        return customerRepository.findById(id).orElseThrow(() ->
+                new EntityNotFoundException("Customer with [" + id + "]; not found. "));
+    }
+
+    private Customer mapToCustomer(CustomerDto cDto, Customer customer) {
         customer.setName(cDto.getName());
         customer.setAddress(cDto.getAddress());
         customer.setMail(cDto.getMail());
         return customer;
     }
-    private CustomerDto mapCustomerToDto(Customer customer){
+
+    private CustomerDto mapCustomerToDto(Customer customer) {
         CustomerDto dto = new CustomerDto();
         dto.setName(customer.getName());
         dto.setAddress(customer.getAddress());
