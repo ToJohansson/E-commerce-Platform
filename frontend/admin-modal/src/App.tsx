86bbec475e2431ccfbabf509260854b8a1a0087c
Tobias Johansson <tobiasp91@hotmail.com>
2024-06-26 16:52:36 +0200

    Add React Router for navigation and update styles and components accordingly

--- frontend/admin-modal/src/App.tsx
+++ frontend/admin-modal/src/App.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductForm from "./Forms/ProductForm";
 import Header from "./Components/Header";
 import LeftNav from "./Components/LeftNav";
 import DisplayContent from "./Components/DisplayContent";
@@ -6,21 +8,37 @@ import "./app.css";
 
 const App = () => {
   return (
-    <div className="container-fluid d-flex flex-column">
-      <div className="row header">
-        <div className="col">
-          <Header />
+    <Router>
+      <div className="container-fluid d-flex flex-column">
+        <div className="row header">
+          <div className="col">
+            <Header />
+          </div>
+        </div>
+        <div className="row flex-grow-1">
+          <div className="col-md-2 leftNav">
+            <LeftNav />
+          </div>
+          <div className="col-md-10 mainContent">
+            <Routes>
+              <Route path="/add-product" element={<ProductForm />} />
+              <Route
+                path="/products"
+                element={<DisplayContent title="Products" />}
+              />
+              <Route
+                path="/orders"
+                element={<DisplayContent title="Orders" />}
+              />
+              <Route
+                path="/customers"
+                element={<DisplayContent title="Customers" />}
+              />
+            </Routes>
+          </div>
         </div>
       </div>
-      <div className="row flex-grow-1 d-flex flex-row">
-        <div className="col-md-2 leftNav d-flex flex-column">
-          <LeftNav />
-        </div>
-        <div className="col-md-10 mainContent d-flex flex-column">
-          <DisplayContent />
-        </div>
-      </div>
-    </div>
+    </Router>
   );
 };
 
