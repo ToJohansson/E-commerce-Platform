3c45c0a15e5437bb153deac7238eee6353873d0d
Tobias Johansson <tobiasp91@hotmail.com>
2024-07-04 20:03:09 +0200

    added React.FC to components

--- frontend/admin-modal/src/Components/Header.tsx
+++ frontend/admin-modal/src/Components/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Header = () => {
+const Header: React.FC = () => {
   return (
     <div>
       <h1>E-Commerce Admin Platform</h1>
--- frontend/admin-modal/src/Components/Products.tsx
+++ frontend/admin-modal/src/Components/Products.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useGetProducts } from "../Hooks/useProducts";
 
 const Products: React.FC = () => {
