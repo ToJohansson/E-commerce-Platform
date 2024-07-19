4659c820873e9759f86e23b299cf630e13a81bb8
Tobias Johansson <tobiasp91@hotmail.com>
2024-07-19 09:21:01 +0200

    customer vercel cors

--- backend/customer/src/main/java/tojohansson/Customer/controllers/CustomerController.java
+++ backend/customer/src/main/java/tojohansson/Customer/controllers/CustomerController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/customers")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "https://e-commerce-platform-sepia.vercel.app"})
 public class CustomerController {
 
     @Autowired
